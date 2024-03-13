d866f522318bc4f971a96a15f7d9540a3bf2ca31
Ego Maragustaf <egomaragustaf@gmail.com>
2024-03-14 01:28:28 +0700

    feat(example): button ui and about route

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -4,7 +4,7 @@ import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Ego Maragustaf" },
+    { title: "About | Ego Maragustaf" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
@@ -13,14 +13,14 @@ export default function Index() {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="max-w-6xl flex flex-col justify-center items-center gap-y-4">
-        <h1 className="text-xl">Welcome to egomaragustaf.com</h1>
+        <h1 className="text-xl">About Me</h1>
         <p className="text-blue-500 text-center max-w-4xl">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio
           numquam harum tempore dolorem nam, id nostrum ipsa reprehenderit est
           culpa iure eligendi nemo, sit et fugit, unde beatae earum eius.
         </p>
         <Button asChild>
-          <Link to="/about">About Me!</Link>
+          <Link to="/">Back to Home</Link>
         </Button>
       </div>
     </div>
