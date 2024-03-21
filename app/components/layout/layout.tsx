925377f62668de5eb0e93153d6e46d81c86b67c4
Ego Maragustaf <egomaragustaf@gmail.com>
2024-03-21 14:06:14 +0700

    feat: add background gradient on intro

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "~/components/layout/navbar";
 import { Footer } from "~/components/layout/footer";
+import { BackgroundGradient } from "~/components/shared/background-gradient";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="max-w-7xl mx-auto flex min-h-screen flex-col">
+      <BackgroundGradient />
       <Navbar />
       <main>{children}</main>
       <Footer />
---
+++ app/components/shared/background-gradient.tsx
@@ -0,0 +1,14 @@
+import { cn } from "~/utils/cn";
+
+export function BackgroundGradient() {
+  return (
+    <div
+      className={cn(
+        "absolute left-0 right-0 -z-10 block h-96 w-full",
+        "bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))]",
+        "from-emerald-200/50 via-emerald-200/10 to-emerald-100/5",
+        "dark:from-emerald-950 dark:via-slate-900/10 dark:to-slate-950/5"
+      )}
+    />
+  );
+}
