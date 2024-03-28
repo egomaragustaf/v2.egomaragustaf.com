f398edb7d49aad0dfa94856c819a689133af5ece
Ego Maragustaf <egomaragustaf@gmail.com>
2024-03-28 12:27:18 +0700

    style: layouting hero section

--- app/components/contents/hero.tsx
+++ app/components/contents/hero.tsx
@@ -4,31 +4,33 @@ import { cn } from "~/utils/cn";
 export function Hero() {
   return (
     <div className="space-y-10">
-      <header>
-        <section className="flex flex-col select-none flex-wrap items-start justify-center gap-2 sm:gap-4">
-          <h3>Nice to meet you! 👋 </h3>
-          <h1 className="text-4xl sm:text-5xl font-bold mb-6">
-            I&apos;m{" "}
-            <span className="underline underline-offset-[16px] decoration-wavy decoration-primary">
-              Ego
-            </span>{" "}
-            Maragustaf
-          </h1>
-          <p className="text-lg font-semibold">
-            a <b className="text-primary">Front-end Developer</b> who design
-            beautifuly simple things, and love what I do
-          </p>
-          <div className="flex gap-6">
-            <Button variant="default">Explore More</Button>
-          </div>
-        </section>
-      </header>
+      <section className="flex flex-col select-none flex-wrap items-start justify-center gap-2">
+        <h3>Nice to meet you! 👋 </h3>
+        <h1 className="text-4xl sm:text-5xl font-bold mb-6">
+          I&apos;m{" "}
+          <span className="underline underline-offset-[12px] decoration-wavy decoration-primary">
+            Ego Maragustaf
+          </span>
+        </h1>
+        <p className="text-lg font-semibold">
+          a <b className="text-primary">Front-end Developer</b> who design
+          beautifuly simple things, and love what I do
+        </p>
+        <div className="flex gap-6 mt-6">
+          <Button variant="default">Explore More</Button>
+        </div>
+      </section>
 
-      <article>
-        <section>
-          <h4>Favorite Tech Stack</h4>
-        </section>
-      </article>
+      <section>
+        <h4>Favorite Tech Stack</h4>
+        <ul className="flex flex-wrap gap-4">
+          <li className="hover:text-primary">React</li>
+          <li className="hover:text-primary">Typescript</li>
+          <li className="hover:text-primary">Remix</li>
+          <li className="hover:text-primary">Next.js</li>
+          <li className="hover:text-primary">Tailwind</li>
+        </ul>
+      </section>
     </div>
   );
 }
