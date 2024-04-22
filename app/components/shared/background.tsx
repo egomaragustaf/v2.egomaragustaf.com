328c44c31f4b3d9c75db23df39b741416992f4ad
Ego Maragustaf <egomaragustaf@gmail.com>
2024-04-22 17:23:04 +0700

    fix: variable color

--- app/components/shared/background.tsx
+++ app/components/shared/background.tsx
@@ -1,6 +1,6 @@
 export function BackgroundGridHero() {
   return (
-    <div className="h-[40rem] -z-20 top-0 left-0 right-0 w-full dark:bg-background bg-white  dark:bg-grid-slate-500/[0.2] bg-grid-background/[0.2] absolute flex items-center justify-center">
+    <div className="h-[40rem] -z-20 top-0 left-0 right-0 w-full dark:bg-background bg-white  dark:bg-grid-slate-600/[0.2] bg-grid-background/[0.2] absolute flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-background bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_10%,background)]"></div>
     </div>
@@ -9,7 +9,7 @@ export function BackgroundGridHero() {
 
 export function BackgroundGridPage() {
   return (
-    <div className="h-[20rem] -z-20 top-0 left-0 right-0 w-full dark:bg-background bg-white  dark:bg-grid-slate-500/[0.2] bg-grid-background/[0.2] absolute flex items-center justify-center">
+    <div className="h-[20rem] -z-20 top-0 left-0 right-0 w-full dark:bg-background bg-white  dark:bg-grid-slate-600/[0.2] bg-grid-background/[0.2] absolute flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-background bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_10%,background)]"></div>
     </div>
@@ -18,7 +18,7 @@ export function BackgroundGridPage() {
 
 export function BackgroundGridFooter() {
   return (
-    <div className="h-96 lg:h-52 -z-20 top-0 left-0 right-0 w-full dark:bg-background bg-white dark:bg-grid-slate-500/[0.2] bg-grid-background/[0.3] absolute flex items-center justify-center">
+    <div className="h-96 lg:h-52 -z-20 top-0 left-0 right-0 w-full dark:bg-background bg-white dark:bg-grid-slate-500/[0.2] bg-grid-background/[0.2] absolute flex items-center justify-center">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-background bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_10%,background)]"></div>
     </div>
